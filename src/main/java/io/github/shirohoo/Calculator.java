2efd0f00f0772efca96dd86a4ce1050de71507eb
shirohoo <shirohoo4102@gmail.com>
2021-11-15 20:46:10 +0900

    :memo: 그레이들 태스크에 대해 작성했다

---
+++ src/main/java/io/github/shirohoo/Calculator.java
@@ -0,0 +1,13 @@
+package io.github.shirohoo;
+
+public class Calculator {
+
+    public int sum(int x, int y) {
+        return x + y;
+    }
+
+    public int minus(int x, int y) {
+        return x - y;
+    }
+
+}
